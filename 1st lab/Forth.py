b74f859ee4df9483163e84da4490c02d36802dac
Ivan-Damyanov <148687259+Ivan-Damyanov@users.noreply.github.com>
2026-05-06 19:12:08 +0300

    Delete test "1st lab directory"

--- 1st lab/Forth.py
+++
@@ -1,9 +0,0 @@
-c = input("Enter c = ")
-d = input("Enter d = ")
-print("Result = ", c + d)
-print(type(c + d))
-
-a = input("Enter a = ")
-b = input("Enter b = ")
-print("Result = ", (a + b) * 3)
-print(type(a + b))
